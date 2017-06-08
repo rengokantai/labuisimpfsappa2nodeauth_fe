a4ae8075dd3733812f04d0ec9863ad478ca2d000
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-08 19:46:01 -0400

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MessagesComponent} from './messages.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent,MessagesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
