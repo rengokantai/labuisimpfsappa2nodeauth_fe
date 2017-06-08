e5b9dc20de0be23f0ea21a4cbb6d2fb81ae4965f
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-08 19:23:21 -0400

    add module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-
+import { MessagesComponent} from './messages.component';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1> <messages></messages>`,
 })
 export class AppComponent  { name = 'Angular'; }
---
+++ src/app/messages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector:'messages',
+    template: 'component'
+})
+
+export class MessagesComponent{}
