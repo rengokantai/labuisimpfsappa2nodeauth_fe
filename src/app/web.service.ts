0352da0774fd3470de7b4e0c3b4283d23c5b31f9
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-10 17:11:41 -0400

    5-5 part1

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,17 +1,24 @@
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import { Injectable} from '@angular/core';
+import {MdSnackBar} from '@angular/material';
 
 @Injectable()
 export class WebService{
     BASE_URL = 'http://localhost:60000/api';
     messages=[];
-    constructor(private http:Http){
+    constructor(private http:Http,private sb:MdSnackBar){
         this.getMessages();
     }
     async getMessages(){
-        var response = await this.http.get(this.BASE_URL+'/messages').toPromise();
-        this.messages = response.json();
+        try {
+            var response = await this.http.get(this.BASE_URL+'/messages').toPromise();
+            this.messages = response.json();
+        } catch (error) {
+            console.error("err")
+            this.sb.open("err",'close',{duration:1000})
+        }
+
     }
     async postMessage(message){
         var response =  await this.http.post(this.BASE_URL+'/messages',message).toPromise();
