6c975f901f507dff364e8682496651df846f9f92
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-10 10:58:48 -0400

    5-1 part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import {NewMessageComponent} from './new-message.component';
 import { AppComponent }  from './app.component';
 import {HttpModule} from '@angular/http';
 import { WebService} from './web.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule,MaterialModule,HttpModule ],
+  imports:      [ BrowserAnimationsModule,BrowserModule,MaterialModule,HttpModule ],
   declarations: [ AppComponent,MessagesComponent,NewMessageComponent ],
   bootstrap:    [ AppComponent ],
   providers: [WebService]
