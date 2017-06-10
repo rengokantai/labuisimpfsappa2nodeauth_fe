dbf3e1d9de0dd6f747a49a17240d26cdc06b8ebd
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-10 16:39:21 -0400

    5-4 part1 refactor

--- src/app/messages.component.ts
+++ src/app/messages.component.ts
@@ -3,7 +3,7 @@ import {WebService} from './web.service';
 
 @Component({
     selector:'messages',
-    template: `<div *ngFor="let message of messages">
+    template: `<div *ngFor="let message of webService.messages">
     <md-card class="card"><md-card-title>{{message.owner}}</md-card-title>
     <md-card-content>{{message.text}}</md-card-content></md-card>`
 })
@@ -11,10 +11,10 @@ import {WebService} from './web.service';
 export class MessagesComponent{
     messages=[];
     constructor(private webService: WebService){}
-    async ngOnInit(){
-        var response = await this.webService.getMessages();
-        this.messages = response.json();
-        console.log(response.json());
-    }
+    // async ngOnInit(){
+    //     var response = await this.webService.getMessages();
+    //     this.messages = response.json();
+    //     console.log(response.json());
+    // }
     //messages = [{text:'a',owner:'ke'},{text:'other',owner:'ma'}]
 }
