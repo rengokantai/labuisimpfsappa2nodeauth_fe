1f8fa8fcc7619bc570840be3c17f4433b618652d
Hernan Y.Ke <yuriqiao@gmail.com>
2017-06-10 22:58:43 -0400

    6-2 part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import {MaterialModule} from '@angular/material';
 import { MessagesComponent} from './messages.component';
 import {NewMessageComponent} from './new-message.component';
 import {NavComponent} from './nav.component';
+import {HomeComponent} from './home.component';
 import { AppComponent }  from './app.component';
 import {HttpModule} from '@angular/http';
 import { WebService} from './web.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 
-var routes =[];
+var routes =[{
+  path:'',
+  component:HomeComponent
+}];
 
 @NgModule({
   imports:      [ BrowserAnimationsModule,BrowserModule,MaterialModule,HttpModule,FormsModule,RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent,MessagesComponent,NewMessageComponent,NavComponent ],
+  declarations: [ AppComponent,MessagesComponent,NewMessageComponent,NavComponent,HomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [WebService]
 })
